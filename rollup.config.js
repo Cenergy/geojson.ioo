a43b74e19192d30a1161d81c827dd65d70575570
Cenergy <673598118@qq.com>
2021-07-18 18:21:07 +0800

    add

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,26 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: ["./src/delegate.js"],
-  output: {
-    file: "./dist/delegate.js",
-    format: "umd",
-    name: "delegate",
+export default [
+  {
+    input: ["./src/delegate.js"],
+    output: {
+      file: "./dist/delegate.js",
+      format: "umd",
+      name: "delegate",
+    },   
+    plugins: [resolve(), commonjs(), babel(), terser()],
   },
-  plugins: [resolve(), commonjs(), babel(), terser()],
-};
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/site.js",
+      format: "cjs",
+      name: "site",
+    },
+    plugins: [resolve()],
+  },
+];
