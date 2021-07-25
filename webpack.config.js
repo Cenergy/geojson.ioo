fb910121f3251647bb955fb2c7b4a7225f1fe691
Cenergy <673598118@qq.com>
2021-07-25 16:45:43 +0800

    modify deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
 /*
  * SplitChunksPlugin is enabled by default and replaced
@@ -22,37 +22,41 @@ const path = require("path");
  *
  */
 
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
+
+const aaa = path.resolve(__dirname, '/');
 
 module.exports = {
   entry: {
     lib: [
-      "/dist/d3.js",
-      "/lib/hashchange.js",
-      "/lib/blob.js",
-      "/lib/base64.js",
-      "/lib/bucket.js",
-      "/lib/queue.js",
-      "/lib/d3.keybinding.js",
-      "/lib/d3.trigger.js",
-      "/lib/d3-compat.js",
-      "/lib/draw/leaflet.draw-src.js",
-      "/lib/drag/leaflet.drag.js",
-      "/lib/codemirror/lib/codemirror.js",
-      "/lib/codemirror/mode/javascript/javascript.js",
-    ]
+      '/dist/d3.js',
+      '/lib/hashchange.js',
+      '/lib/blob.js',
+      '/lib/base64.js',
+      '/lib/bucket.js',
+      '/lib/queue.js',
+      '/lib/d3.keybinding.js',
+      '/lib/d3.trigger.js',
+      '/lib/d3-compat.js',
+      '/lib/draw/leaflet.draw-src.js',
+      '/lib/drag/leaflet.drag.js',
+      '/lib/codemirror/lib/codemirror.js',
+      '/lib/codemirror/mode/javascript/javascript.js',
+    ],
   },
 
   output: {
-    filename: "[name].js",
+    filename: '[name].js',
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   node: {
-    fs: "empty",
+    fs: 'empty',
   },
-  mode: "development",
-  module:{
+  mode: 'development',
+  module: {
     rules: [
       {
         test: /\.js$/,
@@ -61,11 +65,11 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ["@babel/plugin-transform-reserved-words"]
-          }
-        }
-      }
-    ]
+            plugins: ['@babel/plugin-transform-reserved-words'],
+          },
+        },
+      },
+    ],
   },
   optimization: {
     splitChunks: {
@@ -76,10 +80,33 @@ module.exports = {
         },
       },
 
-      chunks: "async",
+      chunks: 'async',
       minChunks: 1,
       minSize: 30000,
       name: true,
     },
   },
+  plugins: [
+    // 自动清空打包的目录
+    new RemovePlugin({
+      after: {
+        root: './public',
+        trash: true
+    }
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'css', to: path.resolve(__dirname, 'public/css') },
+        { from: 'data', to: path.resolve(__dirname, 'public/data') },
+        { from: 'dist', to: path.resolve(__dirname, 'public/dist') },
+        { from: 'img', to: path.resolve(__dirname, 'public/img') },
+        { from: 'leaflet', to: path.resolve(__dirname, 'public/leaflet') },
+        { from: 'lib', to: path.resolve(__dirname, 'public/lib') },
+        {
+          from: 'index.html',
+          to: path.resolve(__dirname, 'public/index.html'),
+        },
+      ],
+    }),
+  ],
 };
